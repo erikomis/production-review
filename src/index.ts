f3fc07fc667ed86b3c6768824036d8af423dfab3
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-06-25 01:22:57 -0300

    express

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,9 @@
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { ErrorApp } from './ErrorApp';
+import { main } from './main';
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof ErrorApp) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-
-    return response
-      .status(500)
-      .json({ status: 'error', message: 'internal  error' });
-  },
-);
-
-app.listen(3333, () => {
-  console.log('server start in port 3333');
-});
+main()
+  .then(() => {
+    console.log('aplicação inicializada');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
